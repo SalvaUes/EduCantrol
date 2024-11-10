23e4daa1f58caf94ee6d792da6be6cc1f0555141
SalvaUes <VL23003@ues.edu.sv>
2024-11-09 18:30:27 -0600

    Inicializacion del proyecto EduCantrol

---
+++ src/main/java/com/educantrol/educantrol_app/EducantrolAppApplication.java
@@ -0,0 +1,13 @@
+package com.educantrol.educantrol_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducantrolAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducantrolAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educantrol/educantrol_app/EducantrolAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educantrol.educantrol_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducantrolAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
