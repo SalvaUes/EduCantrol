7c0292d9bef801200d43f2202b9896b94c3bbb80
unknown <ba22004@ues.edu.sv>
2024-11-12 00:26:46 -0600

    Añadido la entidad curso y modificaciones en la entidad asistencia y expediente academico

--- src/main/java/com/educantrol/educantrol_app/model/HorarioRepository.java
+++ src/main/java/com/educantrol/educantrol_app/model/HorarioRepository.java
@@ -1,7 +1,7 @@
-package com.educantrol.educantrol_app.repository;
+package com.educantrol.educantrol_app.model;
 
-import com.educantrol.educantrol_app.model.Horario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface HorarioRepository extends JpaRepository<Horario, Long> {
 }
--- src/main/java/com/educantrol/educantrol_app/model/ProfesorRepository.java
+++ src/main/java/com/educantrol/educantrol_app/model/ProfesorRepository.java
@@ -1,6 +1,5 @@
-package com.educantrol.educantrol_app.repository;
+package com.educantrol.educantrol_app.model;
 
-import com.educantrol.educantrol_app.model.Profesor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProfesorRepository extends JpaRepository<Profesor, Long> {
